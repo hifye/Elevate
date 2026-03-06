5f53457cb267afc500661eb43de242087431763f
Higor <cearavlogs@gmail.com>
2026-03-06 00:35:03 -0300

    Adicionados métodos esqueleto e comentários `TODO` no `TokenService` para implementação futura de lógica relacionada a Tokens e JWT.

--- src/03-Infrastructure/Infrastructure/Services/TokenService.cs
+++ src/03-Infrastructure/Infrastructure/Services/TokenService.cs
@@ -7,28 +7,34 @@ namespace Infrastructure.Services;
 
 public class TokenService : ITokenService
 {
+    
     public Task<TokenResponse> Token(User user)
     {
+        //TODO: Implementar a lógica de geração do Token
         throw new NotImplementedException();
     }
 
     public string RefreshToken()
     {
+        //TODO: Implementar a lógica de geração do Refresh Token
         throw new NotImplementedException();
     }
-
+    
     public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
     {
+        //TODO: Implementar a lógica de obtenção de informações do Token JWT expirado
         throw new NotImplementedException();
     }
 
     public DateTime TokenExpiration()
     {
+        //TODO: Implementar a lógica de expiração do Token JWT
         throw new NotImplementedException();
     }
 
     public DateTime RefreshTokenExpiration()
     {
+        //TODO: Implementar a lógica de expiração do Refresh Token JWT
         throw new NotImplementedException();
     }
 }
